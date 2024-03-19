b6af3163063e896cb822721324d5cda64ab2bda3
CursedRicky <78096957+CursedRicky@users.noreply.github.com>
2024-03-19 09:33:53 +0100

    Update tombolaServer.py

--- tombolaServer.py
+++ tombolaServer.py
@@ -12,6 +12,7 @@ clients = []
 numeri = []
 for number in range(1, 91):
     numeri.append(number)
+numeriL = []
 
 
 def brodcast(msg) -> None:
@@ -49,21 +50,90 @@ def recive() -> None:
 
 
 def estrai() -> None:
-    global numeri
+    global numeri, numeriL
     value = random.choice(numeri)
     numeri.remove(value)
+    numeriL[value-1].configure(text_color="lime green")
     brodcast(str(value).encode("utf-8"))
 
+def resetta() -> None:
+    global numeriL, numeri
+    for label in numeriL :
+        label.configure(text_color="white")
+    numeri.clear()
+    for number in range(1, 91):
+        numeri.append(number)
+    brodcast("Reset".encode("utf-8"))
 
 def graf() -> None:
+    global numeriL, numeri
     root = customtkinter.CTk()
-    root.geometry("200x200")
     root.title("Tabellone")
+    root.geometry("900x690")
+    root.resizable(False, False)
+    customtkinter.set_default_color_theme("green")
 
-    estaiBtn = customtkinter.CTkButton(master=root, command=estrai, text="Estrai")
-    estaiBtn.pack(pady=5)
+    frame = customtkinter.CTkFrame(master=root)
+    frame.pack(pady=10)
+
+    frameBtn = customtkinter.CTkFrame(master=root, width=200, height=50)
+    frameBtn.pack()
+
+    estraiBtn = customtkinter.CTkButton(master=frameBtn, text="Estrai", width=200, height=50, command=estrai)
+    estraiBtn.grid(pady=10, padx=10, column=0, row=0)
+
+    resetBtn = customtkinter.CTkButton(master=frameBtn, text="Resetta", width=200, height=50, command=resetta, fg_color="red", hover_color="maroon")
+    resetBtn.grid(pady=10, padx=10, column=1, row=0)
+
+    frame1 = customtkinter.CTkFrame(master=frame, width=400)
+    frame1.grid(pady=10, padx=10, column=0, row=0)
+    frame2 = customtkinter.CTkFrame(master=frame, width=400)
+    frame2.grid(pady=10, padx=10, column=1, row=0)
+    frame3 = customtkinter.CTkFrame(master=frame, width=400)
+    frame3.grid(pady=10, padx=10, column=0, row=1)
+    frame4 = customtkinter.CTkFrame(master=frame, width=400)
+    frame4.grid(pady=10, padx=10, column=1, row=1)
+    frame5 = customtkinter.CTkFrame(master=frame, width=400)
+    frame5.grid(pady=10, padx=10, column=0, row=2)
+    frame6 = customtkinter.CTkFrame(master=frame, width=400)
+    frame6.grid(pady=10, padx=10, column=1, row=2)
+
+    i = 0  # Col
+    y = 0  # Row
+    for numero in numeri:
+        if i < 5 and y < 3:
+            label = customtkinter.CTkLabel(text=str(numero), master=frame1, font=("Verdana", 40))
+            label.grid(row=y, column=i, padx=10, pady=5)
+            numeriL.append(label)
+        elif i >= 5 and y < 3:
+            label = customtkinter.CTkLabel(text=str(numero), master=frame2, font=("Verdana", 40))
+            label.grid(row=y, column=i, padx=10, pady=5)
+            numeriL.append(label)
+        elif i < 5 and y < 6:
+            label = customtkinter.CTkLabel(text=str(numero), master=frame3, font=("Verdana", 40))
+            label.grid(row=y, column=i, padx=10, pady=5)
+            numeriL.append(label)
+        elif i >= 5 and y < 6:
+            label = customtkinter.CTkLabel(text=str(numero), master=frame4, font=("Verdana", 40))
+            label.grid(row=y, column=i, padx=10, pady=5)
+            numeriL.append(label)
+        elif i < 5:
+            label = customtkinter.CTkLabel(text=str(numero), master=frame5, font=("Verdana", 40))
+            label.grid(row=y, column=i, padx=10, pady=5)
+            numeriL.append(label)
+        elif i >= 5:
+            label = customtkinter.CTkLabel(text=str(numero), master=frame6, font=("Verdana", 40))
+            label.grid(row=y, column=i, padx=10, pady=5)
+            numeriL.append(label)
+
+        if i < 9:
+            i += 1
+        else:
+            i = 0
+            y += 1
 
     root.mainloop()
+
 
 if __name__ == '__main__':
     print(f"Server Ip: {host}")
